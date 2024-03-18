46abe29741647ff1e629bf03da350c28dd89415d
JK-B0T <JK-B0T@users.noreply.github.com>
2024-03-18 12:54:45 +0100

    Upgrade board generation so it resizes.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Etch-A-Sketch
 The purpose of this project is to practice and demonstrate DOM manipulation skills by making a digital Etch-A-Sketch board.
+
+1.Input is set by the user in 2 text boxes, 1 for the height and 1 for the width. Also has 1 button to resset the grid with set sizes.
+(Grid has to be responsive, more squares means more detail in the board, as they will become smaller).
+
+2.Input is got by the script and sees if it is a number
+and if that number is more than 0 and less or equal to 100.
+
+3.When the button is pressed the grid is generated and all square colors are reset.
+
+4.When the mouse hovers over a square, that square changes to a random color.
+
+5.every square touched is an interaction, and for every interaction all squares not currently black turn 10% more darker.
--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-/* 
-1.Input is set by the user in 2 text boxes, 1 for the height and 1 for the width
-and 1 button to resset the grid.
-(Grid is responsive, more squares means more detail in the board, as they will become smaller,
-this could be achieved with flexbox shrink and growth factors).
-
-2.Input is got by the script and sees if it is a number
-and if that number is more than 0 and less or equal to 100.
-    -If is valid no error is displayed.
-    -If not, an error message is display on the UI.
-
-3.When the button is pressed:
-    -If Inputs are valid, resets the grid with the size specified.
-    -If not, resets the grid but doesn't resizes it.
-
-4.When the mouse hovers over a square, that square changes to a random color.
-
-5.every square touched is an interaction, and for every interaction all saquares
-not currently black turn 10% more darker.
-*/
 "use strict";
 
 const DEFAULT_SIZE = 16;
@@ -68,6 +48,7 @@ function setBoard () {
     const column = document.createElement("div");
     row.classList.add("rowContainer");
     column.classList.add("columnItem");
+
     while (boardContainer.firstChild) {
         boardContainer.removeChild(boardContainer.firstChild);
     }
@@ -79,6 +60,17 @@ function setBoard () {
     for (let i = 0; i < lastRowNum; i++) {
         const rowClone = row.cloneNode(true);
         boardContainer.appendChild(rowClone);
+    }
+
+    if (lastColumnNum === lastRowNum ) {
+        boardContainer.style.width = "90vh";
+        boardContainer.style.height = "90vh";
+    } else if (lastColumnNum > lastRowNum) {
+        const newBoardHeight = 90 * (lastRowNum / lastColumnNum);
+        boardContainer.style.height = newBoardHeight + "vh";
+    } else {
+        const newBoardWidth = 90 * (lastColumnNum / lastRowNum);
+        boardContainer.style.width = newBoardWidth + "vh";
     }
 }
 
